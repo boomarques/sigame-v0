e239f09b1bb4b038d734e89a87539536b32c9b14
marques <marques.python@gmail.com>
2023-03-26 20:20:01 -0300

    commint 00

--- SIGAME/settings.py
+++ SIGAME/settings.py
@@ -70,8 +70,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'static'),
-  os.path.join(BASE_DIR, 'static/css'),
+  BASE_DIR / "static",
+  # os.path.join(BASE_DIR, 'static'),
 ]
 
 
